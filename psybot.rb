dcb1fc033fcc4829751afd0f2491503c46284a24
saf <safiire@gmail.com>
2014-03-14 23:10:33 -0700

    Expanded the capability to respond to strings

--- psybot.rb
+++ psybot.rb
@@ -2,10 +2,26 @@ require 'cinch'
 
 #OPS = %w(vaz SyDy safiire Causai akhentek)
 
+####
+##  Bunch of responses for the bot when he hears a regex
+Responses = {
+
+  /psytrance/i => -> (m){ 
+      m.reply("Psytrance is bad music, #{m.user.nick}") 
+  },
+
+  /yay/i => -> (m){ 
+      m.reply("Yaaaaaaaay!") 
+      m.reply("/me explodes!")
+  }
+}
+
+
 class Psytrance
   include Cinch::Plugin
 
 end
+
 
 bot = Cinch::Bot.new do
   configure do |c|
@@ -18,8 +34,10 @@ bot = Cinch::Bot.new do
     m.channel.op(m.user) unless m.channel.opped? m.user
   end
 
-  on :message, /^.*[pP]sytrance.*$/ do |m|
-    m.reply "Psytrance is bad music, #{m.user.nick}"
+  on :message, /.+/ do |m|
+    Responses.each do |regex, response_function|
+      m.reply(response_function[m]) if regex.match(m.message) != nil
+    end
   end
 end
 
