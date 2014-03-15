0dc3e0c5c0d0017a7dcef9707d24cb369721cd6b
Saf <safiire@gmail.com>
2014-03-15 10:19:41 +0000

    Putting it back to normal

--- psybot.rb
+++ psybot.rb
@@ -4,41 +4,27 @@ require 'cinch'
 
 ####
 ##  Bunch of responses for the bot when he hears a regex
-Responses = {
-
-  /psytrance/i => lambda do |m|
-      m.reply("Psytrance is bad music, #{m.user.nick}") 
-  end,
-
-  /yay/i => lambda do |m|
-      m.reply("Yaaaaaaaay!") 
-      m.reply("/me explodes!")
-  end
-}
-
 
 class Psytrance
   include Cinch::Plugin
 
 end
 
-
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.net"
     c.channels = ["#PsytranceMessiah"]
-    c.nick = ENV['NICK'] || "psybot"
+    c.nick = ENV['NICK'] || "GiR"
   end
 
   on :join do |m|
     m.channel.op(m.user) unless m.channel.opped? m.user
   end
 
-  on :message, /.+/ do |m|
-    Responses.each do |regex, response_function|
-      m.reply(response_function[m]) if regex.match(m.message) != nil
-    end
+  on :message, /psytrance/i do |m|
+    m.reply("Psytrance is bad music")
   end
+
 end
 
 bot.start
