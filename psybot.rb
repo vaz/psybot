bc55d31f36ae1c9cfde40b3dd3e130c9f9b58da1
Vaz Allen <vaz@tryptid.com>
2014-03-17 14:52:55 -0300

    fortune, say, reload, responses hash

--- psybot.rb
+++ psybot.rb
@@ -1,9 +1,9 @@
 require 'cinch'
 
-#OPS = %w(vaz SyDy safiire Causai akhentek)
 
-####
-##  Bunch of responses for the bot when he hears a regex
+CHANNEL = ENV['CHANNEL'] || '#PsytranceMessiah'
+NICK = ENV['NICK'] || 'psykachu'
+
 
 class Fortune
   include Cinch::Plugin
@@ -15,25 +15,65 @@ class Fortune
   end
 end
 
+class Say
+  include Cinch::Plugin
+
+  match(/say (.+)/)
+
+  def execute(m, message)
+    Channel(CHANNEL).send(message)
+  end
+end
+
+class Reload
+  include Cinch::Plugin
+
+  match(/reload/)
+  listen_to(:join, method: :joined)
+
+  def execute(m)
+    @bot.nick += '_reloading'
+    system "git pull"
+    spawn "NICK='#{NICK}' CHANNEL='#{CHANNEL}' ruby #{$0}"
+    @reloading = true
+  end
+
+  def joined(m)
+    if @reloading && m.user.nick == NICK
+      @bot.quit 'I have become obsolete, goodbye.'
+    end
+  end
+end
+
+RESPONSES = {
+  /psy[ -]?trance/i => lambda do |m|
+    unless m.user.user == '~cinch'
+      m.reply "Psytrance is bad music, #{m.user.nick}"
+    end
+  end,
+
+  /ya+y/i => lambda do |m|
+    unless m.user.user == '~cinch'
+      m.reply("Yaaaaaaaaaaay!") 
+      m.action_reply("explodes!")
+    end
+  end
+}
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.net"
-    c.channels = ["#PsytranceMessiah"]
-    c.nick = ENV['NICK'] || "Psykachu"
-    c.plugins.plugins = [Fortune]
+    c.channels = [CHANNEL]
+    c.nick = NICK
+    c.plugins.plugins = [Fortune, Say, Reload]
   end
 
   on :join do |m|
     m.channel.op(m.user) unless m.channel.opped? m.user
   end
 
-  on :message, /psytrance/i do |m|
-     m.reply("Psytrance is bad music, #{m.user.nick}")
-  end
-
-  on :message, /ya+y/i do |m|
-    m.reply("Yaaaaaaaaaaay!")
-    m.action_reply("explodes!")
+  RESPONSES.each do |p, f|
+    on(:message, p) { |m| f[m] }
   end
 
 end
