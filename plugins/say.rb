7d385a91922828f4f893f5c99168f30553eb492c
Vaz Allen <vaz@tryptid.com>
2014-03-27 20:09:53 -0300

    do stuff too

--- plugins/say.rb
+++ plugins/say.rb
@@ -2,9 +2,13 @@ class Say
   include Cinch::Plugin
 
   match(/say (.+)/)
-
   def execute(m, message)
     Channel(CHANNEL).send(message)
+  end
+
+  match(/do (.+)/, method: :execute_action)
+  def execute_action(m, action)
+    Channel(CHANNEL).action(action)
   end
 end
 
